02ff6667120a6f0174c91f8bce727f8aa07eee17
David Guttman <david@davidguttman.com>
2014-02-06 07:13:08 -0800

    pulled out 'single' example

--- example/index.js
+++ example/index.js
@@ -1,11 +1,1 @@
-var LineChart = require('../')
-
-var data = require('./data.json')
-var opts = {
-  width: 500,
-  height: 300,
-  yLabel: 'Price'
-}
-
-var lineChart = new LineChart(data, opts)
-document.body.appendChild(lineChart.el)
+require('./single.js')
